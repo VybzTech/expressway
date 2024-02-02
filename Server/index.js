32e677d11a153ff91c03048fb41c603187749c6d
Dharvo <adedave77@gmail.com>
2024-02-02 13:59:03 +0100

    new file:   Client 	new file:   Server/.gitignore 	new file:   Server/index.js 	new file:   Server/package-lock.json 	new file:   Server/package.json 	new file:   Server/routes/Router.js

---
+++ Server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const router = require(`./routes/Router`);
+// import express from "express";
+// import cors from "cors";
+// import bodyParser from "body-parser";
+
+const app = express();
+
+//  PARSE BODY EXACTLY LIKE THIS
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const corsOpts = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOpts));
+
+//  SETUP ROUTES AFTER CORS & BODY PARSER
+app.use("/", router);
+
+const PORT = 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
