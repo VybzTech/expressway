a29741657516dd01f497c2b6e7d2ca7f685fd1b7
Dharvo <adedave77@gmail.com>
2024-02-08 08:25:17 +0100

    modified:   Client 	deleted:    Server/.env 	modified:   Server/index.js

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,32 @@ const router = require(`./routes/Router`);
 // import cors from "cors";
 // import bodyParser from "body-parser";
 
+// CONNECT THE DATABASE
+const mysql = require("mysql");
+
+// Server=localhost\MSSQLSERVER01;Database=master;Trusted_Connection=True;
+const connection = mysql.createConnection({
+  // connectionUri:
+  // "Server=localhostMSSQLSERVER;Database=master;Trusted_Connection=True;",
+  // host: "localhost\MSSQLSERVER",
+  host: "localhost\\MSSQLSERVER",
+  user: "DHARVO",
+  password: "",
+  database: "LogIn",
+  // database: "master",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err?.code);
+    return;
+  }
+  console.log("Connected to database!");
+});
+
+module.exports = connection;
+
+// INTIALIZEEE THE APPP ON EXPRESS JS BACKEND
 const app = express();
 
 //  PARSE BODY EXACTLY LIKE THIS
@@ -24,7 +50,11 @@ app.use(cors(corsOpts));
 app.use("/", router);
 
 const PORT = 5000;
+// const PORT =process?.env?.PORT|| 5000;
+// console.log(`port: ${process?.env?.PORT}`);
 
 const server = app.listen(PORT, () => {
-  console.log(`Server is running on port: ${process?.env?.PORT ?? PORT}`);
+  console.log(process?.env);
+  console.log(process?.env?.PORT);
+  // console.log(`Server is running on port: '${process?.env}'`);
 });
